e9f20fd52093474e0acd9391c78bcd4a92a1c5cc
DMikhailiets <dmikhailiets21@gmail.com>
2020-04-22 17:41:25 +0300

    api orders 0.1

--- frontend/src/API/api.js
+++ frontend/src/API/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const authAPI = {
     async postUserData(data) {
-        let response = await axios.get('http://gpouk.kbtoffice.co.uk/?Consumer key:=ck_e984e93458c51e617a3b022599e89ac1a964f1d7')
+        let response = await axios.get('http://gpouk.kbtoffice.co.uk/wp-json/wc/v2/orders?Consumer key:=ck_e984e93458c51e617a3b022599e89ac1a964f1d7')
         return {
             response
         };
