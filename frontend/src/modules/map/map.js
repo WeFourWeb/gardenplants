39bcc678ec287fad115fb6ba48a6a90ebdfd1c5c
iliaSNK <shubin.iliasnk@gmail.com>
2020-04-27 08:12:52 +0300

    Add function for draw markers

--- frontend/src/modules/map/map.js
+++ frontend/src/modules/map/map.js
@@ -5,6 +5,8 @@ import style from './map.module.css'
 mapboxgl.accessToken='pk.eyJ1IjoiaWxpYXNuayIsImEiOiJjazk0ZjFsM3AwYWpvM21venRhMHVxZnV0In0.89Hh6UMwZgvHAkbohiT8JQ';
 
 
+
+
 class Maps extends React.Component {
   constructor(props) {
   super(props);
@@ -15,17 +17,36 @@ class Maps extends React.Component {
     };
   }
 
+
   componentDidMount() {
     const map = new mapboxgl.Map({
       container: this.mapContainer,
       style: 'mapbox://styles/mapbox/streets-v11',
       center: [this.state.lng, this.state.lat],
-      zoom: this.state.zoom
+      zoom: this.state.zoom 
     });
-    var marker = new mapboxgl.Marker() 
-    .setLngLat([30.3544, 59.8838])
-    .addTo(map);
-                             
+   
+    
+  let AddMarkers = (Arr) => {
+    for (let i=0; i < Arr.length ; i++){
+      let marker = new mapboxgl.Marker().setLngLat([Arr[i].lng, Arr[i].lat]).addTo(map);
+    }
+  }
+  
+  let DrawMarkers = () => {
+  fetch('http://localhost:1000/api/getAdressList')
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      AddMarkers(data);
+    })
+  }
+   
+    
+    
+
+    DrawMarkers();
     map.on('move', () => {
       this.setState({
         lng: map.getCenter().lng.toFixed(4),
@@ -33,19 +54,15 @@ class Maps extends React.Component {
         zoom: map.getZoom().toFixed(2)
       });
     });
+  
   }
-
+  
   render() {
     return (
       
     
     <div ref={el => this.mapContainer = el} className={style.map_container}/>
-      //<div className={style.map_wrapper}>
-        //  <div className='sidebarStyle'>
-        //   <div>Longitude: {this.state.lng} | Latitude: {this.state.lat} | Zoom: {this.state.zoom}</div>
-        // </div> 
-       
-      //</div>
+     
     )
   }
 }
